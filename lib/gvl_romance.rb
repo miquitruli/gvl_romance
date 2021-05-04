8c8c4c64d67684d713d0dcce55a5e388b445e840
miquitruli <micaelc@g.clemson.edu>
2021-05-04 15:32:10 -0400

    Updated gemspec page

--- lib/gvl_romance.rb
+++ lib/gvl_romance.rb
@@ -4,6 +4,8 @@ require_relative "gvl_romance/version"
 require_relative "gvl_romance/cli"
 require_relative "gvl_romance/scraper"
 
+require 'pry'
+
 module GvlRomance
   class Error < StandardError; end
   # Your code goes here...
